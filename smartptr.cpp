912929aa87d9ba146d661b8541221b50ed9aa50c
dekaiidea <raindekai@hotmail.com>
2018-11-02 16:15:34 +0800

    second commit

--- smartptr.cpp
+++ smartptr.cpp
@@ -6,20 +6,48 @@
 #include "boost/shared_ptr.hpp"
 #include "boost/make_shared.hpp"
 
+struct counted_data
+{
+    int m_count=0;
+};
+
+void intrusive_ptr_add_ref(counted_data *p)
+{
+    ++p->m_count;
+}
+
+void intrusive_ptr_release(counted_data *p)
+{
+    if(--p->m_count==0)
+    {
+        delete p;
+    }
+}
+
 int main()
 {
-    typedef std::vector<boost::shared_ptr<int>> vs;
-    vs v(10);
-    int i=0;
-    for(auto pos=v.begin();pos<v.end();++pos)
-    {
-        *pos = boost::make_shared<int>(++i);
-        std::cout<<*(*pos)<<",";
-    }
-    std::cout<<std::endl;
-    boost::shared_ptr<int>p=v[9];
-    *p=100;
-    std::cout<<*v[9]<<std::endl;
+    typedef boost::intrusive_ptr<counted_data> counted_ptr;
+    counted_ptr p(new counted_data);
+    assert(p);
+    assert(p->m_count==1);
+    counted_ptr p2(p);
+    assert(p->m_count==2);
+    counted_ptr weak_p(p.get(),false);
+    p2.reset();
+    assert(!p2);
+    assert(p->m_count==1);
+    // typedef std::vector<boost::shared_ptr<int>> vs;
+    // vs v(10);
+    // int i=0;
+    // for(auto pos=v.begin();pos<v.end();++pos)
+    // {
+    //     *pos = boost::make_shared<int>(++i);
+    //     std::cout<<*(*pos)<<",";
+    // }
+    // std::cout<<std::endl;
+    // boost::shared_ptr<int>p=v[9];
+    // *p=100;
+    // std::cout<<*v[9]<<std::endl;
     // std::string a = "make_shared";
     // printf("%s" , a.c_str());
     // auto sp = boost::make_shared<std::string>("make_shared");
